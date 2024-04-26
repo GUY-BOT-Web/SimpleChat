797942b182ced7b2077323ec54213493ffc069fa
WebFunSparkTV <167981225+WebFunSparkTV@users.noreply.github.com>
2024-04-26 21:08:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const wss = new WebSocket.Server({ port: 8080 });
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('Received: ', message);
+    var messageData = JSON.parse(message);
+    messageData.username = "anonymous"; // Set username to "anonymous"
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
+        client.send(JSON.stringify(messageData));
       }
     });
   });
