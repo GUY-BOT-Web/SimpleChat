945ff2bfb2d329a965d9a1f19e42a61d1a85ba4e
GUY-BOT-Web <167981225+GUY-BOT-Web@users.noreply.github.com>
2024-04-26 20:45:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    // Broadcast incoming message to all clients
+    console.log('Received: ', message);
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message);
